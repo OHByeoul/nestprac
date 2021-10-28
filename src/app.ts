2056af35fac0b7173e750790b6069b0557561723
오한별 <suriessay@naver.com>
2021-10-29 00:12:21 +0900

    setting

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+//src폴더 안의 app.ts에서 변화가 감지되면 저장하자마자 tsc watch가 감지해서 다시 컴파일해서 node로 실행시켜준다.
+import * as express from "express";
+
+const app: express.Express = express(); // app = express의 인스턴스
+const port: number = 8000;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
